964b3dd31600f9c5f700e51ee99475165c38acb6
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-05-04 02:33:49 +0530

    footer Added

--- tata-1mg/src/App.jsx
+++ tata-1mg/src/App.jsx
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
-import './App.css';
+
+// import './App.css';
 
 import { Navbar } from './components/Navbar';
 
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from './components/Home';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
@@ -17,6 +19,10 @@ function App() {
         <Route path='/home' element={<Home/>} ></Route>
 
       </Routes>
+
+      <Footer/>
+
+
     </div>
   );
 }

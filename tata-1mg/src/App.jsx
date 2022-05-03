4a0ffdfc6edbacee4a52c6458192a44f69562d4b
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-05-03 16:06:03 +0530

    navbar updated

--- tata-1mg/src/App.jsx
+++ tata-1mg/src/App.jsx
@@ -1,11 +1,22 @@
 // import logo from './logo.svg';
 import './App.css';
+
 import { Navbar } from './components/Navbar';
+
+import { Routes, Route } from "react-router-dom";
+
+import { Home } from './components/Home';
 
 function App() {
   return (
     <div className="App">
-           <Navbar/>
+      <Navbar />
+
+      <Routes>
+
+        <Route path='/home' element={<Home/>} ></Route>
+
+      </Routes>
     </div>
   );
 }
---
+++ tata-1mg/src/components/Home.jsx
@@ -0,0 +1,14 @@
+
+
+export const Home = () => {
+
+return <div>
+      
+
+<h1>VAIBHAV</h1>
+
+
+</div>
+
+
+}
